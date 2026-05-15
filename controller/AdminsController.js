293d70a492ad93c3caf62733d5de92b8df2f8da8
TatianaAll <tatiana.allery@orange.fr>
2026-05-15 13:01:07 +0200

    FIX: method update roles

--- controller/AdminsController.js
+++ controller/AdminsController.js
@@ -4,44 +4,45 @@ const jwt = require("jsonwebtoken");
 
 // Update the roles for a user
 exports.updateRoles = (req, res, next) => {
+  console.log("controller Admin")
   const id = req.params.id;
 
   // Control: only admins can updates the roles
-  if (!req.auth || req.auth.role !== includes("admin")) {
+  if (!req.auth || !req.auth.role.includes("admin")){
     return res.status(403).json({ message: "Non autorisé" });
   }
 
-  const role = req.body;
-
+  const role = req.body.role;
   // Check if a role is passed
   if (!role) {
     return res.status(400).json({ message: "Rôle manquant" });
   }
 
+  // Format of role need to be an array
+  const roles = Array.isArray(role) ? role : [role];
+
   // whitelist of roles and check the validity
   const allowedRoles = ["user", "jury", "admin"];
   // The every() method of Array instances returns false if it finds an element in the array that does not satisfy the provided testing function. Otherwise, it returns true.
-  const isValid = role.every((r) => allowedRoles.includes(r));
+  const isValid = roles.every((r) => allowedRoles.includes(r));
   if (!isValid) {
     return res.status(400).json({ message: "Rôle invalide" });
   }
 
   // An admin cannot unadmin himself
-  if (req.auth.userId === id && !role.includes("admin")) {
+  if (req.auth.userId === id && !roles.includes("admin")) {
     return res.status(400).json({
       message: "Vous ne pouvez pas vous retirer vous-même votre rôle admin",
     });
   }
 
-  Users.findByIdAndUpdate(id, role, { new: true, runValidators: true })
+  Users.findByIdAndUpdate(id, { role: roles }, { new: true, runValidators: true })
     .then((rolesUpdated) => {
       if (!rolesUpdated)
         return res.status(404).json({ message: "Utilisateur non trouvé" });
       res.status(200).json(rolesUpdated);
     })
     .catch((error) =>
-      resizeTo
-        .status(400)
-        .json({ message: "Erreur lors de la mise à jour des rôles" }),
+      res.status(400).json({ message: "Erreur lors de la mise à jour des rôles" }),
     );
 };
--- routes/adminsRoute.js
+++ routes/adminsRoute.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const UsersController = require("../controller/UsersController");
+const AdminsController = require("../controller/AdminsController");
 const auth = require("../middleware/auth.js");
 
 // route to update the role only available for admins
-router.put("/users/:id/roles", UsersController.updateRoles);
+router.put("/users/:id/roles", auth, AdminsController.updateRoles);
+
+module.exports = router;
